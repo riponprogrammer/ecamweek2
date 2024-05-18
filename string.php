6a18f1a4f175d9872ea17758cd60dfb330cd33eb
ripon <riponray91193@gmail.com>
2024-05-18 15:23:27 +0600

    first commit

---
+++ string.php
@@ -0,0 +1,23 @@
+<?php
+$strings = ["Hello", "World", "PHP", "Programming"];
+$vowels = array("a", "e", "i", "o", "u");
+
+$n = count($strings);
+
+for($i=0; $i<$n; $i++){
+     $str = $strings[$i];
+      $strlen = strlen($str);
+      $count = 0;
+      for ($j=0; $j<$strlen; $j++) {
+      if (in_array(strtolower($str[$j]), $vowels)) {
+        $count++;
+      }
+    }
+  echo "Original String: ".$str.", Vowel Count: ".$count.", Reversed String: ",strrev($str)."\n";
+}
+
+
+
+
+
+?>
